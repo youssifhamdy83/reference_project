ed19fbab558f36b12c03e2210214529f96dbd559
Youssif <youssifhamdy83@gmail.com>
2024-08-04 15:52:08 +0300

    vs pushing to master

---
+++ lib/doc_app.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:reference_project/core/routing/app_router.dart';
+import 'package:reference_project/core/theming/colors.dart';
+
+import 'core/routing/routes.dart';
+
+class DocApp extends StatelessWidget {
+  final AppRouter appRouter;
+
+  const DocApp({super.key, required this.appRouter});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        designSize: const Size(375, 812),
+        minTextAdapt: true,
+        child: MaterialApp(
+          title: 'Reference Project',
+          theme: ThemeData(
+            primaryColor: ColorsManager.mainBlue,
+            scaffoldBackgroundColor: Colors.white,
+          ),
+          debugShowCheckedModeBanner: false,
+          initialRoute: Routes.onBoardingScreen,
+          onGenerateRoute: appRouter.generateRoute,
+        ));
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,15 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:reference_project/core/routing/app_router.dart';
+import 'package:reference_project/doc_app.dart';
 
 import 'package:reference_project/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(DocApp(appRouter: AppRouter(),));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
